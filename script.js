6a363208be03e9f20f8145d862962fde6f901438
Prince Lakhwani <princelakhwani1608@gmail.com>
2025-11-06 11:19:33 +0530

    prince

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ const navLinks = document.querySelectorAll('#navbar a');
 navLinks.forEach((link) => {
   link.addEventListener('click', () => {
     navLinks.forEach((l) => l.classList.remove('active'));
-    link.classList.add('active');
   });
 });
 
